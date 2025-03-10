30c45bffb991069edf9e121a1ba1217f03aba6de
Kamran Ali <ely.kamran@gmail.com>
2025-03-10 21:37:49 +0500

    assets

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apihost: 'http://localhost:8080',
+  imageAPI: '/image',
+  loginAPI: '/login',
+  userAPI: '/user',
+  eventAPI: '/event',
+  sessionToken: 'API_KEY',
+  useDBForBoardStorage: false,
+  demo: true,
+  applicationTitle:'NGX Dynamic Dashboard'
+};
